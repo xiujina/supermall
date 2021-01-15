5ae0335e354ba3fadb0a30ab94c89a6aed921704
jin <1025330456@qq.com>
2021-01-15 14:49:13 +0800

    home swiper

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="tab-bar">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+  box-shadow: 0 -1px 3px rgba(100,100,100,.3);
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  
+}
+
+</style>
